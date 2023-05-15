24a0f8073a7e265b1d1b295052caad4b14a73646
Matthew Ernst <6628@holbertonstudents.com>
2023-05-15 13:56:53 -0700

    up[date

--- io_redirections_and_filters/pushit.sh
+++ io_redirections_and_filters/pushit.sh
@@ -2,7 +2,7 @@
 echo "Hello, Tell me what your pushing :)"
 read RESPONSE
 chmod u+x *
-git add.
+git add .
 git commit -m $RESPONSE
 git push
 echo "Thank you, You have sent with the message $RESPONSE unless it failed"
